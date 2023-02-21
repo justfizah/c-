0b77caeac138dff98d00a5d65bf78e4c87034cb6
justfizah <hafeexahabdul@gmsil.com>
2023-02-21 08:03:10 -0800

    h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,15 @@
  * main - prints the string "Holberton" from a character array.
  * Return: 0 on success.
  */
+
 int main(void)
 {
-char c[] = "_putchar";
-int i;
-for (i = 0; i < 9; i++)
-{
-_putchar(c[i]);
-}
-_putchar('\n');
-return (0);
+	char *sh = "_putchar";
+	while (*sh)
+	{
+	_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+	return (0);
 }
