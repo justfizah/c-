cec94a46c17514b3f79bef0eaffe52f4045378ef
justfizah <hafeexahabdul.com>
2023-03-31 00:45:27 -0700

    linked

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,13 +7,12 @@
  */
 void free_list(list_t *head)
 {
-  list_t *tmp;
-
-  while (head)
-    {
-      tmp = head->next;
-      free(head->str);
-      free(head);
-      head = tmp;
-    }
+list_t *tmp;
+while (head)
+{
+tmp = head->next;
+free(head->str);
+free(head);
+head = tmp;
+}
 }
